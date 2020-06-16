cb967d3b41af372a18bdbd91892e4ad1ddd734e8
proativarastreamento <adm@proativarastreamento.com>
2020-06-16 14:30:13 -0300

    Desafio 02 Conceitos do Node.js

---
+++ src/app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get('/repositories', (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+
+  const { title, url, techs, likes=0 } = request.body;
+
+  const repositorie = { id: uuid(), title, url , techs, likes };
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
+  
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } =  request.params;
+  const { title, url, techs} = request.body;
+ 
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: "Project not found." });
+  };
+
+  const repo = {
+    id,
+    title,
+    url,
+    techs,
+    likes:repositories[repoIndex].likes
+  }
+  
+  repositories[repoIndex] = repo;
+
+
+  return response.json(repo);
+
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+    const repoIndex = repositories.findIndex(repo => repo.id === id);
+  
+    if (repoIndex < 0) {
+
+        return response.status(400).json({ error: "Project not found." })
+    }
+    repositories.splice(repoIndex, 1);
+
+    return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } =  request.params;
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+
+    return response.status(400).json({ error: "Project not found." })
+}
+repositories[repoIndex].likes += 1;
+return response.json(repositories[repoIndex]);
+  
+});
+
+module.exports = app;
